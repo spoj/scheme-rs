2de77471bb84df19c114f74aa92eb90d32c02d28
Matthew Wong <yuyeung.wong@gmail.com>
2025-07-28 17:11:40 +0800

    sexp hand written parser. initial list paring

---
+++ src/main.rs
@@ -0,0 +1,76 @@
+#[derive(Debug)]
+enum Sexp {
+    Number(usize),
+    List(Vec<Sexp>),
+}
+
+struct Parser<'a> {
+    inner: &'a str,
+    cursor: usize,
+}
+
+impl<'a> Parser<'a> {
+    fn new(inner: &'a str) -> Self {
+        let cursor = 0;
+        Self { inner, cursor }
+    }
+    fn peek(&mut self) -> Option<&'a str> {
+        if self.cursor < self.inner.len() {
+            Some(&self.inner[self.cursor..self.cursor + 1])
+        } else {
+            None
+        }
+    }
+    fn consume_if<P>(&mut self, pred: P) -> Option<&'a str>
+    where
+        P: Fn(&'a str) -> bool,
+    {
+        if let Some(x) = self.peek()
+            && pred(x)
+        {
+            self.cursor += 1;
+            Some(x)
+        } else {
+            None
+        }
+    }
+    fn parse_number(&mut self) -> Option<Sexp> {
+        if let Ok(num) = self.inner[self.cursor..self.cursor + 1].parse::<usize>() {
+            self.cursor += 1;
+            Some(Sexp::Number(num))
+        } else {
+            None
+        }
+    }
+    fn consume_spaces(&mut self) {
+        while self.consume_if(|s| s == " ").is_some() {}
+    }
+    fn consume_right_paren(&mut self) -> bool {
+        self.consume_if(|s| s == ")").is_some()
+    }
+    fn parse_list(&mut self) -> Option<Sexp> {
+        let mut inner: Vec<Sexp> = vec![];
+        match &self.inner[self.cursor..self.cursor + 1] {
+            "(" => {
+                self.cursor += 1;
+                self.consume_spaces();
+                while let Some(s) = self.parse_sexp() {
+                    inner.push(s);
+                    self.consume_spaces();
+                }
+                self.consume_right_paren();
+                Some(Sexp::List(inner))
+            }
+            _ => None,
+        }
+    }
+    fn parse_sexp(&mut self) -> Option<Sexp> {
+        self.parse_number().or_else(|| self.parse_list())
+    }
+}
+
+fn main() {
+    let input = "(1 2 (1 2 3))";
+    let mut parser = Parser::new(input);
+    dbg!(parser.parse_sexp());
+}
