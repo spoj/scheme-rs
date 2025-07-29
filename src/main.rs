4cbd5971839066ea2fa5a622edfc444cd346d179
Matthew Wong <yuyeung.wong@gmail.com>
2025-07-29 21:57:48 +0800

    implement eval

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,5 @@
+use std::collections::HashMap;
+
 use nom::Parser;
 use nom::branch::alt;
 use nom::bytes::complete::tag;
@@ -18,7 +20,7 @@ fn symbol(input: &str) -> IResult<&str, &str> {
     })
     .parse(input)
 }
-fn sexp(input: &str) -> IResult<&str, Sexp> {
+pub fn sexp(input: &str) -> IResult<&str, Sexp> {
     alt((map(atom, Sexp::Atom), map(list, Sexp::List))).parse(input)
 }
 
@@ -35,19 +37,77 @@ fn atom(input: &str) -> IResult<&str, Atom> {
 
 #[allow(unused)]
 #[derive(Debug)]
-enum Sexp {
+pub enum Sexp {
     Atom(Atom),
     List(Vec<Sexp>),
 }
 
+impl Sexp {
+    fn as_atom(&self) -> Option<&Atom> {
+        match self {
+            Sexp::Atom(atom) => Some(atom),
+            Sexp::List(_) => None,
+        }
+    }
+}
+
+impl Sexp {
+    pub fn eval(&self, env: HashMap<String, Value>) -> Option<Value> {
+        match self {
+            Sexp::Atom(Atom::Number(n)) => Some(Value::Number(*n)),
+            Sexp::Atom(Atom::Symbol(n)) => env.get(n).cloned(),
+            Sexp::List(sexps) if sexps.is_empty() => None,
+            Sexp::List(sexps)
+                if sexps
+                    .first()
+                    .and_then(|s| s.as_atom())
+                    .and_then(|a| a.as_atom_str())
+                    == Some("add") =>
+            {
+                let res: isize = sexps[1..]
+                    .iter()
+                    .flat_map(|s| s.as_atom().and_then(|a| a.as_number()))
+                    .sum();
+                Some(Value::Number(res))
+            }
+            _ => unreachable!(),
+        }
+    }
+}
+
 #[allow(unused)]
 #[derive(Debug)]
-enum Atom {
+pub enum Atom {
     Number(isize),
     Symbol(String),
+}
+
+impl Atom {
+    fn as_atom_str(&self) -> Option<&str> {
+        match self {
+            Atom::Number(_) => None,
+            Atom::Symbol(n) => Some(n),
+        }
+    }
+    fn as_number(&self) -> Option<isize> {
+        match self {
+            Atom::Number(n) => Some(*n),
+            Atom::Symbol(_) => None,
+        }
+    }
+}
+
+#[allow(unused)]
+#[derive(Debug, Clone)]
+pub enum Value {
+    Number(isize),
 }
 
 fn main() {
     let input = "(3 (2 3 -34 3) +5 ()) ";
     let _ = dbg!(sexp(input));
+    let input = "(add 1 2 3 4 5 6)";
+    let _ = dbg!(sexp(input));
+    let result = sexp(input).unwrap().1.eval(Default::default());
+    dbg!(result);
 }
