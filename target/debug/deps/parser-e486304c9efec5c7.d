64288f7f95b579e10a5769933dee5399f31f3752
Matthew Wong <yuyeung.wong@gmail.com>
2025-07-29 21:57:48 +0800

    untrack target files

--- target/debug/deps/parser-e486304c9efec5c7.d
+++
@@ -1,5 +0,0 @@
-/home/spoj/src/parser/target/debug/deps/parser-e486304c9efec5c7.d: src/main.rs
-
-/home/spoj/src/parser/target/debug/deps/libparser-e486304c9efec5c7.rmeta: src/main.rs
-
-src/main.rs:
