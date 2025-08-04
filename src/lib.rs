fa9b200bffc82a2e2522e1224586b9d58994c14d
Matthew Wong <yuyeung.wong@gmail.com>
2025-08-04 20:41:35 +0800

    add run_lisp function

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,12 @@
+use crate::{lisp::Value, parse::sexp};
 pub mod lisp;
 pub mod parse;
 
 #[cfg(test)]
 mod tests;
+
+pub fn run_lisp(program: &str) -> Option<Value> {
+    let (_, sexp) = sexp(program).ok()?;
+    let env = Default::default();
+    sexp.eval(&env)
+}
--- src/tests/mod.rs
+++ src/tests/mod.rs
@@ -1,17 +1,14 @@
 use crate::lisp::*;
 use crate::parse::*;
+use crate::run_lisp;
 use std::collections::HashMap;
 #[test]
 fn test_simple_add() {
-    let input = "(add 1 2 3 4 5 6)";
-    let result = sexp(input).unwrap().1.eval(&Default::default());
-    assert_eq!(result, Some(Value::Number(21)));
+    assert_eq!(run_lisp("(add 1 2 3 4 5 6)"), Some(Value::Number(21)));
 }
 #[test]
 fn test_complex_add() {
-    let input = "(add (add 4 5 6) 1 2 3)";
-    let result = sexp(input).unwrap().1.eval(&Default::default());
-    assert_eq!(result, Some(Value::Number(21)));
+    assert_eq!(run_lisp("(add (add 4 5 6) 1 2 3)"), Some(Value::Number(21)));
 }
 #[test]
 fn test_env() {
@@ -25,51 +22,41 @@ fn test_env() {
 
 #[test]
 fn test_lambda() {
-    let input = "((lambda (a) (add a a)) 7)";
-    let result = sexp(input).unwrap().1.eval(&Default::default());
-    assert_eq!(result, Some(Value::Number(14)));
+    assert_eq!(
+        run_lisp("((lambda (a) (add a a)) 7)"),
+        Some(Value::Number(14))
+    );
 }
 #[test]
 fn test_lambda2() {
-    let input = "((lambda (f x) (f (f x))) (lambda (a) (add a a)) 3)";
-    let result = sexp(input).unwrap().1.eval(&Default::default());
-    assert_eq!(result, Some(Value::Number(12)));
+    assert_eq!(
+        run_lisp("((lambda (f x) (f (f x))) (lambda (a) (add a a)) 3)"),
+        Some(Value::Number(12))
+    );
 }
 #[test]
 fn test_eq1() {
-    let input = "(equal)";
-    let result = sexp(input).unwrap().1.eval(&Default::default());
-    assert_eq!(result, Some(Value::Number(1)));
+    assert_eq!(run_lisp("(equal)"), Some(Value::Number(1)));
 }
 #[test]
 fn test_eq2() {
-    let input = "(equal 2)";
-    let result = sexp(input).unwrap().1.eval(&Default::default());
-    assert_eq!(result, Some(Value::Number(1)));
+    assert_eq!(run_lisp("(equal 2)"), Some(Value::Number(1)));
 }
 #[test]
 fn test_eq3() {
-    let input = "(equal 2 3)";
-    let result = sexp(input).unwrap().1.eval(&Default::default());
-    assert_eq!(result, Some(Value::Number(0)));
+    assert_eq!(run_lisp("(equal 2 3)"), Some(Value::Number(0)));
 }
 #[test]
 fn test_eq4() {
-    let input = "(equal 2 2)";
-    let result = sexp(input).unwrap().1.eval(&Default::default());
-    assert_eq!(result, Some(Value::Number(1)));
+    assert_eq!(run_lisp("(equal 2 2)"), Some(Value::Number(1)));
 }
 #[test]
 fn test_eq5() {
-    let input = "(equal 2 2 2 2)";
-    let result = sexp(input).unwrap().1.eval(&Default::default());
-    assert_eq!(result, Some(Value::Number(1)));
+    assert_eq!(run_lisp("(equal 2 2 2 2)"), Some(Value::Number(1)));
 }
 #[test]
 fn test_eq6() {
-    let input = "(equal 2 2 2 2 1)";
-    let result = sexp(input).unwrap().1.eval(&Default::default());
-    assert_eq!(result, Some(Value::Number(0)));
+    assert_eq!(run_lisp("(equal 2 2 2 2 1)"), Some(Value::Number(0)));
 }
 #[test]
 fn test_all_same1() {
@@ -103,27 +90,22 @@ fn test_all_same5() {
 
 #[test]
 fn test_cond1() {
-    let input = "(cond (0 1) (2 3))";
-    let result = sexp(input).unwrap().1.eval(&Default::default());
-    assert_eq!(result, Some(Value::Number(3)));
+    assert_eq!(run_lisp("(cond (0 1) (2 3))"), Some(Value::Number(3)));
 }
 #[test]
 fn test_cond2() {
-    let input = "(cond (1 1) (2 3))";
-    let result = sexp(input).unwrap().1.eval(&Default::default());
-    assert_eq!(result, Some(Value::Number(1)));
+    assert_eq!(run_lisp("(cond (1 1) (2 3))"), Some(Value::Number(1)));
 }
 #[test]
 fn test_cond3() {
-    let input = "(cond (0 1) (0 3))";
-    let result = sexp(input).unwrap().1.eval(&Default::default());
-    assert_eq!(result, Some(Value::Number(0)));
+    assert_eq!(run_lisp("(cond (0 1) (0 3))"), Some(Value::Number(0)));
 }
 #[test]
 fn test_cond4() {
-    let input = "(cond ((equal 1 2) 1) ((equal 4 4) 3))";
-    let result = sexp(input).unwrap().1.eval(&Default::default());
-    assert_eq!(result, Some(Value::Number(3)));
+    assert_eq!(
+        run_lisp("(cond ((equal 1 2) 1) ((equal 4 4) 3))"),
+        Some(Value::Number(3))
+    );
 }
 
 // Z combinator is fixed point combinator for pass by value systems. Same as Y combinator but
@@ -132,58 +114,53 @@ fn test_cond4() {
 #[test]
 fn test_recur_parse1() {
     let ycomb = "(lambda(f)((lambda (x) (f (lambda (v) ((x x) v)))) (lambda (x) (f (lambda (v) ((x x) v))))))";
-    let result = sexp(ycomb).unwrap().1.eval(&Default::default());
-    assert!(result.is_some());
+    assert!(run_lisp(ycomb).is_some());
 }
 
 #[test]
 fn test_recur_parse2() {
     let fib = "(lambda (f) (lambda (n) (cond ((equal n 0) (1)) ((equal n 1) (1)) (1 (add (f (add n -1)) (f (add n -2)))))))";
-    let result = sexp(fib).unwrap().1.eval(&Default::default());
-    assert!(result.is_some());
+    assert!(run_lisp(fib).is_some());
 }
 
 #[test]
 fn test_recur_fib() {
     let ycomb = "(lambda(f)((lambda (x) (f (lambda (v) ((x x) v)))) (lambda (x) (f (lambda (v) ((x x) v))))))";
     let fib = "(lambda (f) (lambda (n) (cond ((equal n 0) 1) ((equal n 1) 1) (1 (add (f (add n -1)) (f (add n -2)))))))";
-    // let fib = "(lambda (f) (lambda (n) (cond ((equal n 0) 1) ((equal n 1) 1) (1 (f 0)))))";
-
-    let program = format!("(({} {}) {})", ycomb, fib, 0);
-    let result = sexp(&program).unwrap().1.eval(&Default::default());
-    assert_eq!(result, Some(Value::Number(1)));
-
-    let program = format!("(({} {}) {})", ycomb, fib, 1);
-    let result = sexp(&program).unwrap().1.eval(&Default::default());
-    assert_eq!(result, Some(Value::Number(1)));
-
-    let program = format!("(({} {}) {})", ycomb, fib, 2);
-    let result = sexp(&program).unwrap().1.eval(&Default::default());
-    assert_eq!(result, Some(Value::Number(2)));
-
-    let program = format!("(({} {}) {})", ycomb, fib, 3);
-    let result = sexp(&program).unwrap().1.eval(&Default::default());
-    assert_eq!(result, Some(Value::Number(3)));
-
-    let program = format!("(({} {}) {})", ycomb, fib, 4);
-    let result = sexp(&program).unwrap().1.eval(&Default::default());
-    assert_eq!(result, Some(Value::Number(5)));
-
-    let program = format!("(({} {}) {})", ycomb, fib, 5);
-    let result = sexp(&program).unwrap().1.eval(&Default::default());
-    assert_eq!(result, Some(Value::Number(8)));
-
-    let program = format!("(({} {}) {})", ycomb, fib, 6);
-    let result = sexp(&program).unwrap().1.eval(&Default::default());
-    assert_eq!(result, Some(Value::Number(13)));
+    assert_eq!(
+        run_lisp(&format!("(({} {}) {})", ycomb, fib, 0)),
+        Some(Value::Number(1))
+    );
+    assert_eq!(
+        run_lisp(&format!("(({} {}) {})", ycomb, fib, 1)),
+        Some(Value::Number(1))
+    );
+    assert_eq!(
+        run_lisp(&format!("(({} {}) {})", ycomb, fib, 2)),
+        Some(Value::Number(2))
+    );
+    assert_eq!(
+        run_lisp(&format!("(({} {}) {})", ycomb, fib, 3)),
+        Some(Value::Number(3))
+    );
+    assert_eq!(
+        run_lisp(&format!("(({} {}) {})", ycomb, fib, 4)),
+        Some(Value::Number(5))
+    );
+    assert_eq!(
+        run_lisp(&format!("(({} {}) {})", ycomb, fib, 5)),
+        Some(Value::Number(8))
+    );
+    assert_eq!(
+        run_lisp(&format!("(({} {}) {})", ycomb, fib, 6)),
+        Some(Value::Number(13))
+    );
 }
 
 #[test]
 fn test_list1() {
-    let program = "(list 1 2 (add 3 4))";
-    let result = sexp(program).unwrap().1.eval(&Default::default());
     assert_eq!(
-        result,
+        run_lisp("(list 1 2 (add 3 4))"),
         Some(Value::List(vec![
             Value::Number(1),
             Value::Number(2),
@@ -194,40 +171,42 @@ fn test_list1() {
 
 #[test]
 fn test_list2() {
-    let program = "(car (list 1 2 (add 3 4)))";
-    let result = sexp(program).unwrap().1.eval(&Default::default());
-    assert_eq!(result, Some(Value::Number(1)));
+    assert_eq!(
+        run_lisp("(car (list 1 2 (add 3 4)))"),
+        Some(Value::Number(1))
+    );
 }
 
 #[test]
 fn test_list3() {
-    let program = "(cdr (list 1 2 (add 3 4)))";
-    let result = sexp(program).unwrap().1.eval(&Default::default());
     assert_eq!(
-        result,
+        run_lisp("(cdr (list 1 2 (add 3 4)))"),
         Some(Value::List(vec![Value::Number(2), Value::Number(7)]))
     );
 }
 
 #[test]
 fn test_list4() {
-    let program = "(empty (cdr (list 1 2 (add 3 4))))";
-    let result = sexp(program).unwrap().1.eval(&Default::default());
-    assert_eq!(result, Some(Value::Number(0)));
+    assert_eq!(
+        run_lisp("(empty (cdr (list 1 2 (add 3 4))))"),
+        Some(Value::Number(0))
+    );
 }
 
 #[test]
 fn test_list5() {
-    let program = "(empty (cdr (cdr (list 1 2 (add 3 4)))))";
-    let result = sexp(program).unwrap().1.eval(&Default::default());
-    assert_eq!(result, Some(Value::Number(0)));
+    assert_eq!(
+        run_lisp("(empty (cdr (cdr (list 1 2 (add 3 4)))))"),
+        Some(Value::Number(0))
+    );
 }
 
 #[test]
 fn test_list6() {
-    let program = "(empty (cdr (cdr (cdr (list 1 2 (add 3 4))))))";
-    let result = sexp(program).unwrap().1.eval(&Default::default());
-    assert_eq!(result, Some(Value::Number(1)));
+    assert_eq!(
+        run_lisp("(empty (cdr (cdr (cdr (list 1 2 (add 3 4))))))"),
+        Some(Value::Number(1))
+    );
 }
 
 #[test]
@@ -237,6 +216,5 @@ fn test_list7() {
     let data = "(list 1 2 (add 3 4))";
 
     let program = format!("(({ycomb} {len}) {data})");
-    let result = sexp(&program).unwrap().1.eval(&Default::default());
-    assert_eq!(result, Some(Value::Number(3)));
+    assert_eq!(run_lisp(&program), Some(Value::Number(3)));
 }
